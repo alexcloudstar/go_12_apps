69298d04a98b4f712c3ee0b3ec4fbb9be2c45d32
alexcloudstar <alexcloudstar@gmail.com>
2024-05-16 21:43:04 +0300

    feat: Get User Input & Show Feedback

--- task_manager/main.go
+++ task_manager/main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hi! Please choose an option:")
+	fmt.Println("1. Add a new task")
+	fmt.Println("2. List all tasks")
+	fmt.Println("3. Mark a task as completed")
+	fmt.Println("4. Delete a task")
+
+	for {
+		var option int
+		fmt.Scanln(&option)
+
+		switch option {
+		case 1:
+			fmt.Println("Please enter the task description:")
+		case 2:
+			fmt.Println("Here are all the tasks:")
+		case 3:
+			fmt.Println("Please enter the task number you want to mark as completed:")
+		case 4:
+			fmt.Println("Please enter the task number you want to delete:")
+		default:
+			fmt.Println("Goodbye!")
+			return
+		}
+	}
 }
