0f0357e3c20b22fc33ff42887256612ffd9b9c81
alexcloudstar <alexcloudstar@gmail.com>
2024-05-16 21:48:18 +0300

    feat: Task Struct, printInitMsg() func

--- task_manager/main.go
+++ task_manager/main.go
@@ -1,17 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+type Task struct {
+	ID          int    `json:"id"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+}
 
 func main() {
 	fmt.Println("Hi! Please choose an option:")
-	fmt.Println("1. Add a new task")
-	fmt.Println("2. List all tasks")
-	fmt.Println("3. Mark a task as completed")
-	fmt.Println("4. Delete a task")
+
+	printInitMsg()
+	os.Create("tasks.json")
 
 	for {
 		var option int
 		fmt.Scanln(&option)
+
+		if option < 1 || option > 5 {
+			fmt.Println("Invalid option. Please choose a valid option.")
+			printInitMsg()
+			continue
+		}
 
 		switch option {
 		case 1:
@@ -27,4 +41,12 @@ func main() {
 			return
 		}
 	}
+}
+
+func printInitMsg() {
+	fmt.Println("1. Add a new task")
+	fmt.Println("2. List all tasks")
+	fmt.Println("3. Mark a task as completed")
+	fmt.Println("4. Delete a task")
+	fmt.Println("5. Exit")
 }
