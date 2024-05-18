b102d677694235a35596977414c63c7503e1e0ba
alexcloudstar <alexcloudstar@gmail.com>
2024-05-18 16:05:52 +0300

    refactor: fileops

--- task_manager/fileops/fileops.go
+++ task_manager/fileops/fileops.go
@@ -1,4 +1,43 @@
 package fileops
 
-func S() {
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/alexcloudstar/go_12_apps/task_manager/duties"
+)
+
+const File_name = "tasks.json"
+const File_mode = 0644
+
+func Init(tasks *[]duties.Duty) []byte {
+	content, err := os.ReadFile(File_name)
+
+    if errors.Is(err, os.ErrNotExist) {
+        file, _ := os.Create(File_name)
+
+        file.Sync()
+    }
+
+	if err != nil {
+		fmt.Println("Could not read the tasks file")
+		return nil
+	}
+
+	if len(content) > 0 {
+		err = json.Unmarshal(content, &tasks)
+	}
+
+    return content 
+}
+
+func Write(tasks *[]duties.Duty) error {
+    json, _ := json.Marshal(&tasks)
+
+
+    err := os.WriteFile(File_name, json, File_mode)
+
+    return err
 }
--- task_manager/main.go
+++ task_manager/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 
 	"github.com/alexcloudstar/go_12_apps/task_manager/duties"
+	"github.com/alexcloudstar/go_12_apps/task_manager/fileops"
 	"github.com/alexcloudstar/go_12_apps/task_manager/utils"
 )
 
 const file_name = "tasks.json"
-const osPerm = os.O_CREATE | os.O_WRONLY
 const file_mode = 0644
 
 func main() {
@@ -18,30 +16,8 @@ func main() {
     tasks := duties.Init()
 
     utils.ShowOptions()
-	file, err := os.OpenFile(file_name, osPerm, file_mode)
 
-	defer file.Close()
-
-	if err != nil {
-		fmt.Println("Could not create the tasks file")
-		return
-	}
-
-	content, err := os.ReadFile(file_name)
-
-	if err != nil {
-		fmt.Println("Could not read the tasks file")
-		return
-	}
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	if len(content) > 0 {
-		err = json.Unmarshal(content, &tasks)
-	}
+    content := fileops.Init(&tasks)
 
 	for {
 		var option int
@@ -51,14 +27,12 @@ func main() {
 		case 1:
             tasks := duties.New(tasks)
 
-			json, _ := json.Marshal(&tasks)
-
-			err := os.WriteFile(file_name, json, file_mode)
+            err := fileops.Write(&tasks)
 
 			if err != nil {
 				fmt.Println("Could not write to the tasks file")
-				fmt.Println(err)
 			}
+
 		case 2:
 			fmt.Println("Here are all the tasks:")
 			fmt.Println(string(content))
@@ -69,9 +43,7 @@ func main() {
 
              tasks := duties.ToggleCompleted(tasks, task_id)
 
-            json, _ := json.Marshal(&tasks)
-
-            err := os.WriteFile(file_name, json, file_mode)
+            err := fileops.Write(&tasks)
 
             if err != nil {
                 fmt.Println("could not write to the tasks file")
@@ -84,9 +56,8 @@ func main() {
 			fmt.Scanln(&task_id)
 
             tasks := duties.DeleteDuty(tasks, task_id)
-            json, _ := json.Marshal(&tasks)
 
-            err := os.WriteFile(file_name, json, file_mode)
+            err := fileops.Write(&tasks)
 
             if err != nil {
                 fmt.Println("Could not write to the tasks file")
