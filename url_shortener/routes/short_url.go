c47fa6832e7559825ff5574081579327b43d441c
alexcloudstar <alexcloudstar@gmail.com>
2024-05-23 21:37:56 +0300

    feat: get url from body, hash it, and get it

--- url_shortener/routes/short_url.go
+++ url_shortener/routes/short_url.go
@@ -1,15 +1,48 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/alexcloudstar/go_12_apps/url_shortener/utils"
 	"github.com/gin-gonic/gin"
 )
 
+type Url struct {
+    Short string `json:"short"`
+    Long string `json:"long"`
+}
+
+type Body struct {
+    Url string `json:"url"`
+}
+
+var urls []Url
+
 func RedirectUrl(ctx *gin.Context) {
-    ctx.JSON(http.StatusOK, "hello world")
+    ctx.JSON(http.StatusOK, gin.H{
+        "urls": urls,
+    })
 }
 
 func ShortUrl(ctx *gin.Context) {
-    ctx.JSON(http.StatusOK, "hello world")
+    var body Body
+
+    err := ctx.BindJSON(&body)
+
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+
+    hashedUrl := utils.GetHash(5)
+
+    urls = append(urls, Url{
+        Short: hashedUrl,
+        Long: body.Url,
+    })
+
+    ctx.JSON(http.StatusOK, gin.H{
+        "url": hashedUrl,
+    })
 }
